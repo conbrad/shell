7c2656ea358afe481678786b7e97ed8b923f0fe7
Conor <con.brad@gmail.com>
2013-10-01 11:49:06 -0700

    Working initial version

--- shell.c
+++ shell.c
@@ -1,16 +1,54 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #define BUFFER_LENGTH 512
+void time_prompt(){
+  /* this is what our child process will use as output */
+  int sout;
+  /* attempt to fork new process */
+    switch (fork()) {
+      case -1:
+             //error;
+      case 0:
+        dup2(sout, STDOUT_FILENO);
+        close(sout);
+	/* process runs date program */
+        execlp("date", "date", NULL);
+    }
+}
+
 int main(int argc, char **argv, char **envp)
 {
-  char buffer[BUFFER_LENGTH];
-  char* path = "/bin"; 
+ char buffer[BUFFER_LENGTH];
+ char* path = "/bin/";
+ char* ls = "/bin/ls";
   while(1)
-  {    
-     //print the prompt
-     printf("myShell&gt;");
-     //get input
+  {
+    /* print the prompt */
+    time_prompt();
+    
+    char* arg_list = NULL;
+        
+    /* get input */
      fgets(buffer, BUFFER_LENGTH, stdin);
+     buffer[strlen(buffer) -1 ] = '\0';
+     char* command;     
+     command = malloc(strlen(ls));
+     strcpy(command, path);
+     strcat(command, buffer);
+     printf("the command is: %s", command);
+     if (strcmp(ls, command) == 0)
+       {
+	 printf("And they are equal!\n");
+       }
+     else
+       {
+	 printf("they not equal\n");
+       }
      int pid = fork();
      if(pid !=0)
        {
@@ -18,8 +56,10 @@ int main(int argc, char **argv, char **envp)
        }
      else
        {
-	 int rv = execv("/bin/date", argv);
+	 int rv = execv(command, argv);
+	 printf("code from execv is: %i", rv);
        }
-  }  
+  }   
   return 0;
 }
+
